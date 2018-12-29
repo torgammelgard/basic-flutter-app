9c8b31382d5669ac2b68990b238379cc1bd7c64c
Tor Gammelgård <tor.gammelgard@gmail.com>
2018-12-29 02:39:58 +0100

    Add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _planets = ['Saturn from list ok'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,11 +19,31 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Amazing title'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[Image.asset('assets/saturn.jpg'), Text('Saturn')],
+        body: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              child: Text('add planet'),
+              onPressed: () {
+                setState(() {
+                  _planets.add('Hardcoded planet');
+                  print(_planets);
+                });
+              },
+            ),
           ),
-        ),
+          Column(
+              children: _planets
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/saturn.jpg'),
+                            Text(element)
+                          ],
+                        ),
+                      ))
+                  .toList())
+        ]),
       ),
     );
   }
